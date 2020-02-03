df6ffac62c5443be3a1be60024aa7b98097f6d81
odooaktiv <odoo@aktivsoftware.com>
2020-02-03 18:22:37 +0530

    [Change] some changes with module 13.0.1.0.1

--- customer_search_filter/__manifest__.py
+++ customer_search_filter/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'customer',
     'license': 'AGPL-3',
     'website': 'www.aktivsoftware.com',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.0.1',
     'summary': '''
                 This module helps in finding a customer with respect to its
                 mobile number, phone number, city,
